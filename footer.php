0fa8e5a3d1ada329f5cdf322c552a6c4317478d1
marksdsd <118698496+marksdsd@users.noreply.github.com>
2022-11-21 09:18:53 +0800

    footer.php源码如下
    footer.php源码如下

---
+++ footer.php
@@ -0,0 +1,29 @@
+<div class="row">
+
+
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bk5">
+<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+<ul>
+<li class="phone"><?php echo $info[0]['company_phone']?><span>Email:<?php echo $info[0]['company_email']?></span></li>
+<li class="address"><?php echo $info[0]['company_address']?></li>
+<li class="ltd"><?php echo $info[0]['company_name']?></li>
+<li class="link" style="margin-top:10px;"><img src="image/icon4.png" style="vertical-align: middle;margin-top:-20px;"/></li>
+</ul>
+
+</div>
+<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"></div>
+    </div>
+</div>
+
+
+ </div>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="" async defer></script>
+    </body>
+</html>
+
